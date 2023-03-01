62fff1ede2372884a486aa2f5ac6be8814430fb1
amaluald_INFO <atormalual@gmail.com>
2023-02-28 21:37:09 -0500

    Connected the login page to the welcome page

---
+++ app/src/main/java/com/example/pro_q/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.pro_q;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button loginButton;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.email_login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,ContactMainActivity.class ));
+            }
+        });
+    }
+}
+
+
+
+
+
